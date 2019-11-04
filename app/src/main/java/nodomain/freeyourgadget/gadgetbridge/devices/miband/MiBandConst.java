56a0bf984cfcc36fc0a0836de800a78f02387cbc
Christopher H. Casebeer <christopher@chc.name>
2019-11-04 00:43:08 -0500

    Limit heuristic 'Mi' prefix, since the Mijia Quartz Watch is named 'Mi_Watch'

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/miband/MiBandConst.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/miband/MiBandConst.java
@@ -70,7 +70,7 @@ public final class MiBandConst {
 
     public static final String ORIGIN_INCOMING_CALL = "incoming_call";
     public static final String ORIGIN_ALARM_CLOCK = "alarm_clock";
-    public static final String MI_GENERAL_NAME_PREFIX = "MI";
+    public static final String MI_GENERAL_NAME_PREFIX = "MIB";
     public static final String MI_1 = "1";
     public static final String MI_1A = "1A";
     public static final String MI_1S = "1S";
